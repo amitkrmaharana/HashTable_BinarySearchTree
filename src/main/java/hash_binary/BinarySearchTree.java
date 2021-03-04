eba17125c3135c09e0fb73cd0c82141cdfb85860
amitkrmaharana <ak657harley@gmail.com>
2021-03-04 08:41:49 +0530

    Refactored: To search 63 in binary tree

--- src/main/java/hash_binary/BinarySearchTree.java
+++ src/main/java/hash_binary/BinarySearchTree.java
@@ -4,32 +4,43 @@ public class BinarySearchTree<K extends Comparable<K>> {
 
     public BinaryTreeNode<K> root;
 
-    public void add(K key){
+    public void add(K key) {
         this.root = this.addRecursively(root, key);
     }
 
-    public BinaryTreeNode<K> addRecursively(BinaryTreeNode<K> current, K key){
-        if (current == null){
+    public BinaryTreeNode<K> addRecursively(BinaryTreeNode<K> current, K key) {
+        if (current == null) {
             return new BinaryTreeNode<>(key);
         }
         int result = key.compareTo(current.key);
-        if (result == 0){
+        if (result == 0) {
             return current;
         }
-        if (result == -1){
+        if (result < 0 ) {
             current.left = addRecursively(current.left, key);
-        }else if (result == 1){
+        } else if (result > 0) {
             current.right = addRecursively(current.right, key);
         }
         return current;
     }
 
-    public int getSizeRecursively(BinaryTreeNode<K> current){
-        return current == null ? 0 : 1 + this.getSizeRecursively(current.left)+
+    public int getSizeRecursively(BinaryTreeNode<K> current) {
+        return current == null ? 0 : 1 + this.getSizeRecursively(current.left) +
                 this.getSizeRecursively(current.right);
     }
 
-    public int getSize(){
+    public int getSize() {
         return this.getSizeRecursively(root);
     }
+
+    public K nodeSearch(BinaryTreeNode<K> current, K key) {
+        if (current == null || current.key == key)
+            return key;
+        int result = key.compareTo(current.key);
+        if (result < 0)
+            return nodeSearch(current.left, key);
+        else
+            return nodeSearch(current.right, key);
+    }
+
 }
